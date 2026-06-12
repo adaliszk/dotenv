7293a061ead2774d84044ef9fcb32d2660e74365
Ádám Liszkai <adam@liszkai.dev>
2026-06-13 01:22:26 +0200

    chore: Switch to Dprint formatter

--- flake.nix
+++ flake.nix
@@ -1,38 +1,64 @@
 {
-  description = "Nix Profiles by AdaLiszk, btw";
+  description = "Nix System by AdaLiszk, btw";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
+        devTools = with pkgs; [
+          git
+          dprint
+          nixfmt
+          nufmt
+        ];
         profiles = builtins.attrNames (builtins.readDir ./profiles);
         importProfile = name: import (./profiles + "/${name}.nix") { inherit pkgs; };
-        paths = builtins.listToAttrs (map (name: {
-          inherit name;
-          value = importProfile name;
-        }) profiles);
-      in {
-        packages = builtins.listToAttrs (map (name: {
-          inherit name;
-          value = pkgs.buildEnv {
-            name = name;
-            paths = paths.${name};
+        paths = builtins.listToAttrs (
+          map (name: {
+            inherit name;
+            value = importProfile name;
+          }) profiles
+        );
+      in
+      {
+        packages =
+          builtins.listToAttrs (
+            map (name: {
+              inherit name;
+              value = pkgs.buildEnv {
+                name = name;
+                paths = paths.${name};
+              };
+            }) profiles
+          )
+          // {
+            devTools = pkgs.buildEnv {
+              name = "devtools";
+              paths = devTools;
+            };
           };
-        }) profiles) // {
-          everything = pkgs.buildEnv {
-            name = "default";
-            paths = builtins.concatLists (builtins.attrValues profiles);
-          };
+        defaultPackage = self.packages.devTools;
+
+        devShells.default = pkgs.mkShell {
+          packages = devTools;
+          shellHook = ''
+            dprint fmt
+          '';
         };
-        defaultPackage = self.packages.everything;
       }
     );
 }
