045ddca0d71ef0fdc80db6d162817e8ebe85e134
Ádám Liszkai <adam@liszkai.dev>
2026-06-12 15:39:43 +0200

    refactor: reset essentials to the bare minimum

--- profiles/pkgs-essentials.nix
+++ profiles/pkgs-essentials.nix
@@ -1,25 +1,14 @@
 { pkgs }: with pkgs;
 
 [
-  neofetch
+  hyfetch
   nushell
-  nushellPlugins.query
-  nushellPlugins.units
-  nushellPlugins.skim
-  nushellPlugins.highlight
   nufmt
   stow
   tldr
   git
   git-lfs
-  openssh
   rsync
   zip
   unzip
-  docker
-  docker-buildx
-  docker-sync
-  docker-ls
-  docker-gc
-  lazydocker
 ]
