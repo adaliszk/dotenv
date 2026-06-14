85e1d6ba6522a82153f1d00665f10c7f4fa18045
Ádám Liszkai <adam@liszkai.dev>
2026-06-14 20:15:55 +0200

    remove: drop greetd from nix

--- systems/hyprland.nix
+++ systems/hyprland.nix
@@ -17,7 +17,6 @@ let
     environment.systemPackages = with pkgs; [
       hyprland
       tuigreet # FUTURE: Switch when https://github.com/hyprwm/hyprlock/pull/731
-      greetd
     ];
 
     environment.etc."greetd/config.toml".text = ''
@@ -27,31 +26,6 @@ let
         command = "${greetCommand}"
         user = "greeter"
     '';
-
-    environment.etc."pam.d/greetd".text = ''
-      auth      include   system-local-login
-      account   include   system-local-login
-      session   include   system-local-login
-      password  include   system-local-login
-    '';
-
-    systemd.services.greetd = {
-      enable = true;
-      wantedBy = [ "system-manager.target" ];
-      after = [
-        "systemd-user-sessions.service"
-        "getty@tty1.service"
-      ];
-      conflicts = [ "getty@tty1.service" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.greetd}/bin/greetd";
-        Restart = "always";
-        StandardInput = "tty";
-        TTYPath = "/dev/tty1";
-        TTYReset = true;
-        TTYVHangup = true;
-      };
-    };
   };
 in
 {
