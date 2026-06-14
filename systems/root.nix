1369e601ba638105b3e1c0cf09355c3bd2276250
Ádám Liszkai <adam@liszkai.dev>
2026-06-14 08:36:34 +0200

    feat: configure trusted users

--- systems/root.nix
+++ systems/root.nix
@@ -8,6 +8,7 @@
 rec {
   config = {
     nixpkgs.hostPlatform = system;
+    nix.settings.trusted-users = "root @wheel";
     nix.settings.experimental-features = [
       "nix-command"
       "flakes"
