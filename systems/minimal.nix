fefd74be172c6341bc67184472ae3754d613afdf
Ádám Liszkai <adam@liszkai.dev>
2026-06-15 23:33:00 +0200

    refactor: use nu with foot only

--- systems/minimal.nix
+++ systems/minimal.nix
@@ -9,21 +9,12 @@ let
   config = {
     nixpkgs.hostPlatform = system;
     environment.systemPackages = with pkgs; [
-      nushell
       git
       git-lfs
       fastfetch
       rsync
       stow
     ];
-    environment.etc."shells".text = ''
-      /bin/sh
-      /bin/bash
-      /usr/bin/sh
-      /usr/bin/bash
-      /usr/bin/systemd-home-fallback-shell
-      ${pkgs.nushell}
-    '';
   };
 in
 {
