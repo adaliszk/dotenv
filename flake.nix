08600360752a2c228933fd42a717b31556ed7061
Ádám Liszkai <adam@liszkai.dev>
2026-06-17 22:26:11 +0200

    feat: add NixGL wrapper script

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:numtide/system-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixGL = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -16,27 +20,37 @@
       nixpkgs,
       systemManager,
       flakeUtils,
+      nixGL,
       ...
     }:
     flakeUtils.lib.eachDefaultSystem (
       system:
       let
-        lib = nixpkgs.lib;
         pkgs = import nixpkgs {
           config.allowUnfree = true;
           inherit system;
         };
-        systemNames = map (lib.removeSuffix ".nix") (builtins.attrNames (builtins.readDir ./systems));
-        profileNames = map (lib.removeSuffix ".nix") (builtins.attrNames (builtins.readDir ./profiles));
-        importNix = dir: name: import (dir + "/${name}.nix") { inherit pkgs system systemManager; };
-        profiles = lib.genAttrs profileNames (importNix ./profiles);
+        systemNames = map (pkgs.lib.removeSuffix ".nix") (builtins.attrNames (builtins.readDir ./systems));
+        profileNames = map (pkgs.lib.removeSuffix ".nix") (builtins.attrNames (builtins.readDir ./profiles));
+        nixGLWrap = nixGL: pkg: pkgs.symlinkJoin {
+          name = "${pkg.pname or pkg.name}-nixgl";
+          paths = [ pkg ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            for bin in $out/bin/*; do
+              if [ -L "$bin" ]; then
+                tgt=$(readlink -f "$bin"); rm "$bin"
+                makeWrapper ${nixGL}/bin/nixGL "$bin" --add-flags "$tgt"
+              fi
+            done
+          '';
+        };
+        importNix = dir: name: import (dir + "/${name}.nix") { inherit pkgs system systemManager nixgl nixGLWrap; };
+        profiles = pkgs.lib.genAttrs profileNames (importNix ./profiles);
       in
       {
         systemConfigs = lib.genAttrs systemNames (name: (importNix ./systems name).system);
-        packages = profiles // {
-          default = profiles.essentials;
-        };
-
+        packages = profiles // { default = profiles.essentials; };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             dprint
