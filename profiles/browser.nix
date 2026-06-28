35c0ef80829c940e440a4f04664f22cc55a9b5e5
Ádám Liszkai <adam@liszkai.dev>
2026-06-28 16:59:36 +0200

    feat(browser): add settings and password -manager

--- profiles/browser.nix
+++ profiles/browser.nix
@@ -18,11 +18,20 @@ let
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/matte-black-spring-green/latest.xpi";
           installation_mode = "force_installed";
         };
+        "passff@invicem.pro" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/passff/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
     extraPrefs = ''
+      defaultPref("browser.startup.page", 3); // Restore Session
       lockPref("browser.startup.homepage", "about:blank");
       lockPref("extensions.activeThemeID", "{78a5c5f7-7289-43c2-b420-3197d7a7557c}");
+      defaultPref("sidebar.revamp", true);
+      defaultPref("sidebar.verticalTabs", true);
+      defaultPref("sidebar.visibility", "expand-on-hover");
+      defaultPref("sidebar.main.tools", "bookmarks");
     '';
   };
 in
