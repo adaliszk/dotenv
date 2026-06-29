f996a1f9d84a0f7260ab4abaa3d6cb0f0098d17d
Ádám Liszkai <adam@liszkai.dev>
2026-06-29 09:24:00 +0200

    feat(agentdev): configure opencode with local models

--- flake.nix
+++ flake.nix
@@ -61,7 +61,6 @@
         systemConfigs = systems;
         packages = profiles // {
           default = profiles.essentials;
-          lazyllama = pkgs.lazyllama;
         };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
